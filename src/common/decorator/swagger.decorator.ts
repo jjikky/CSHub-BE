b7b7dbd52302bd14acb0dc2bc4482c87e82240eb
jjikky <kok5417@skuniv.ac.kr>
2025-01-07 20:50:59 +0900

    feat: 페이징처리 응답 데코레이터 추가 #43

--- src/common/decorator/swagger.decorator.ts
+++ src/common/decorator/swagger.decorator.ts
@@ -12,6 +12,50 @@ export const ApiGetResponse = <TModel extends Type<any>>(model: TModel) => {
   );
 };
 
+export const ApiGetItemsResponse = <TModel extends Type<any>>(
+  model: TModel,
+) => {
+  return applyDecorators(
+    ApiOkResponse({
+      schema: {
+        allOf: [
+          {
+            properties: {
+              items: {
+                type: 'array',
+                items: {
+                  $ref: getSchemaPath(model),
+                },
+              },
+              page: {
+                type: 'number',
+                description: '현재 페이지',
+                example: 1,
+              },
+              limit: {
+                type: 'number',
+                description: '페이지당 항목 수',
+                example: 10,
+              },
+              total: {
+                type: 'number',
+                description: '전체 데이터 수',
+                example: 192,
+              },
+              total_page: {
+                type: 'number',
+                description: '전체 페이지 수',
+                example: 20,
+              },
+            },
+            required: ['items', 'page', 'limit'],
+          },
+        ],
+      },
+    }),
+  );
+};
+
 export const ApiPostResponse = <TModel extends Type<any>>(model: TModel) => {
   return applyDecorators(
     ApiCreatedResponse({
